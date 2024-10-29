c767b381cb5fe97bf64758cd8376b53cba4372cb
bliss06 <deepaksingh.b@gmail.com>
2024-10-29 00:58:00 -0400

    Completed lynt adherence

--- server.py
+++ server.py
@@ -1,27 +1,34 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector  
-import json
+from EmotionDetection.emotion_detection import emotion_detector
 
-#Initiate the flask app 
+#Initiate the flask app
 app = Flask("Emotion detector")
 
 @app.route("/emotionDetector")
-def runAnalysis():
+def run_nalysis():
+    ''' This function performs the sentiment analysis '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
 
-    print(response)
-    anger_score, disgust_score, sadness_score, fear_score, joy_score, dominant_emotion = response["anger"], response["disgust"], response["sadness"], response["fear"], response["joy"], response["dominant_emotion"]
-    
+    anger_score, disgust_score, sadness_score, fear_score, joy_score, dominant_emotion = \
+    response["anger"], response["disgust"], response["sadness"], response["fear"], \
+    response["joy"], response["dominant_emotion"]
+
     # Check if the label is None, indicating an error or invalid input
-    if text_to_analyze == "" or dominant_emotion == None:
+    if text_to_analyze == "" or dominant_emotion is None:
         return "Invalid text! Please try again!."
-    else:
-        # Extract the scores and dominant emotion
-        response_string = f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
-        return response_string
+
+    # Extract the scores and dominant emotion
+    response_string = f"For the given statement, the system response is 'anger': \
+    {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and \
+    'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
+    return response_string
 
 @app.route("/")
 def render_index_page():
@@ -29,5 +36,4 @@ def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000'''
     app.run()
